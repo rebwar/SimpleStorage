4ff5f0d813a0bd714f919a873b26a87ff5df30b4
Rebwar Hosein Poori <rebwar.poori@gmail.com>
2023-09-23 13:23:32 +0330

    Add Struct and Mapping

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,27 +1,33 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.10;
+contract SimpleStorage{
+     uint256 public myFavoriteNumber=5;
+     struct Person{
+          uint256 favoriteNumber;
+          string name;
+     }
+     mapping(string=>uint256) public nameToFavoriteNumber;
+     Person[] public allPeople;
+     function store(uint256 _favoriteNumber) public{
+          myFavoriteNumber = _favoriteNumber;
+     }
 
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+     function retrive() public view returns(uint256){
+          return myFavoriteNumber;
+     }
 
-contract SimpleStorage {
-    uint256 myFavoriteNumber=1;
-    struct Person{
-        uint256 favoriteNumber;
-        string name;
-    }
+     function addPeople(uint256 _favoriteNumber,string memory _name) public{
+          allPeople.push(Person(_favoriteNumber,_name));
+          nameToFavoriteNumber[_name]=_favoriteNumber;
+     }
 
-    Person[] public  people;
-    function store(uint256 _myFavoriteNumber) public {
-        myFavoriteNumber = _myFavoriteNumber;
-    }
+     // function addNumMemory(uint256[] memory _nums) public{
+     //      for(uint i=0;i<_nums.length;i++){
+     //           _nums(i)=i;
+     //      }
+     // }
+     
 
-    function retrive() public view returns(uint256) {
-        return myFavoriteNumber;
-    }
+     
 
-    function  addPerson(uint256 _favoriteNumber,string memory _name) public  {
-        Person memory   person=Person(_favoriteNumber,_name);
-        people.push(person);
-       // people.push(Person(_favoriteNumber,_name));
-
-    }
 }
